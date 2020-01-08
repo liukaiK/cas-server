6251f629858532be4df6fd98812510237e5db8c8
liukai <zhizhufan@foxmail.com>
2020-01-08 14:37:43 +0800

    feat: update

--- src/main/java/com/unicom/smartcity/CasServerApplication.java
+++ src/main/java/com/unicom/smartcity/CasServerApplication.java
@@ -1,12 +1,16 @@
-//package com.unicom.smartcity;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//@SpringBootApplication
-//public class CasServerApplication {
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(CasServerApplication.class, args);
-//    }
-//}
+package com.unicom.smartcity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liukai
+ */
+@SpringBootApplication
+public class CasServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CasServerApplication.class, args);
+    }
+
+}

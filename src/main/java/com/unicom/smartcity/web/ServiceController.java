886b6dda1faf15a2f4469c81d95e65106ddea798
liukai <zhizhufan@foxmail.com>
2020-01-08 14:22:04 +0800

    feat: 配置service

---
+++ src/main/java/com/unicom/smartcity/web/ServiceController.java
@@ -0,0 +1,20 @@
+//package com.unicom.smartcity.web;
+//
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+///**
+// * @author liukai
+// */
+//@RestController
+//@RequestMapping("/services")
+//public class ServiceController {
+//
+//
+//    @GetMapping
+//    public String he() {
+//        return "hello";
+//    }
+//
+//}

35c1f356eb3b3329cbfac802618a6e67a2cb3b7c
liukai <zhizhufan@foxmail.com>
2020-01-15 15:53:45 +0800

    feat: grant_type增加rest

--- src/main/java/com/unicom/smartcity/security/oauth2/OAuth2AuthorizationServerConfigurer.java
+++ src/main/java/com/unicom/smartcity/security/oauth2/OAuth2AuthorizationServerConfigurer.java
@@ -10,8 +10,14 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.ClientDetailsService;
+import org.springframework.security.oauth2.provider.CompositeTokenGranter;
+import org.springframework.security.oauth2.provider.TokenGranter;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * OAuth2 授权服务
@@ -41,6 +47,7 @@ public class OAuth2AuthorizationServerConfigurer extends AuthorizationServerConf
         endpoints.authenticationManager(authenticationManager);
         endpoints.tokenStore(jwtTokenStore);
         endpoints.tokenEnhancer(jwtAccessTokenConverter);
+        endpoints.tokenGranter(tokenGranter(endpoints));
     }
 
     @Override
@@ -48,6 +55,14 @@ public class OAuth2AuthorizationServerConfigurer extends AuthorizationServerConf
         security.checkTokenAccess("isAuthenticated()");
         security.tokenKeyAccess("isAuthenticated()");
     }
+
+
+    private TokenGranter tokenGranter(final AuthorizationServerEndpointsConfigurer endpoints) {
+        List<TokenGranter> granters = new ArrayList<>(Collections.singletonList(endpoints.getTokenGranter()));
+        granters.add(new OAuth2RestTokenGranter(authenticationManager, endpoints.getTokenServices(), endpoints.getClientDetailsService(), endpoints.getOAuth2RequestFactory()));
+        return new CompositeTokenGranter(granters);
+    }
+
 
     @Bean
     public ClientDetailsService JPAClientDetailsService() {

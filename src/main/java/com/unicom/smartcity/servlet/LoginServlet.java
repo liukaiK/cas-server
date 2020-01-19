6401bb68cac4c8ff54a254e3962505f9bc21e355
liukai <zhizhufan@foxmail.com>
2020-01-19 15:18:44 +0800

    feat: 增加log4j配置

--- src/main/java/com/unicom/smartcity/servlet/LoginServlet.java
+++ src/main/java/com/unicom/smartcity/servlet/LoginServlet.java
@@ -1,12 +1,14 @@
 package com.unicom.smartcity.servlet;
 
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.logging.Logger;
 
 /**
  * 登录servlet
@@ -15,7 +17,8 @@ import java.util.logging.Logger;
  */
 public class LoginServlet extends HttpServlet {
 
-    private Logger logger = Logger.getLogger(this.getClass().toString());
+
+    private Log log = LogFactory.getLog(this.getClass());
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -25,9 +28,9 @@ public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String username = request.getParameter("username");
-        logger.info("username: " + username);
+        log.info("username: " + username);
         String password = request.getParameter("password");
-        logger.info("password: " + password);
+        log.info("password: " + password);
     }
 
 

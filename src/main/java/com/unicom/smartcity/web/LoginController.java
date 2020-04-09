b3eb4de713305c9ed67d612863326ff20d68fddc
liukai <zhizhufan@foxmail.com>
2020-04-09 08:48:26 +0800

    feat: update

--- src/main/java/com/unicom/smartcity/web/LoginController.java
+++ src/main/java/com/unicom/smartcity/web/LoginController.java
@@ -2,6 +2,7 @@ package com.unicom.smartcity.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -17,7 +18,10 @@ public class LoginController {
      * @param clientId 在系统中认证过的应用的clientId
      */
     @GetMapping("/login")
-    public String login(@RequestParam(value = "client_id") String clientId, Model model) {
+    public String login(@RequestParam(value = "client_id", required = false) String clientId, Model model) {
+        if (StringUtils.isEmpty(clientId)) {
+
+        }
         model.addAttribute("clientId", clientId);
         return "login";
     }

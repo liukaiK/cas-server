7bafb367ae01daf809c90bbe2b8ead79dac9db06
liukai <zhizhufan@foxmail.com>
2020-05-27 09:34:20 +0800

    JWT 修改为内存存储

--- src/main/java/com/unicom/smartcity/security/oauth2/OAuth2TokenConfigurer.java
+++ src/main/java/com/unicom/smartcity/security/oauth2/OAuth2TokenConfigurer.java
@@ -2,9 +2,8 @@ package com.unicom.smartcity.security.oauth2;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
 /**
  * 配置token存储和JWT
@@ -15,8 +14,8 @@ import org.springframework.security.oauth2.provider.token.store.redis.RedisToken
 public class OAuth2TokenConfigurer {
 
     @Bean
-    public TokenStore tokenStore(RedisConnectionFactory redisConnectionFactory) {
-        return new RedisTokenStore(redisConnectionFactory);
+    public TokenStore tokenStore() {
+        return new InMemoryTokenStore();
     }
 
 }
